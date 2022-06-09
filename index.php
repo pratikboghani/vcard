882c7b95f2e8d191968f48b98003b35138581adb
pratikboghani1 <78678091+pratikboghani1@users.noreply.github.com>
2022-06-09 23:18:20 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,263 @@
+
+<!-- Techbit Infotech -->
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Pratik Boghani</title>
+    <link rel="icon" href="D:\pratik\vcard\pb-blue-black1.jpg" />
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="reset.min.css" />
+    <link rel='stylesheet prefetch' href='font-awesome.min.css'>
+    <!--
+ <script type='text/javascript' src='assets/js/sharethis.js#property=5bf6ae289b95fc00123f97b1&product=sticky-share-buttons' async='async'></script> -->
+    <script src='jquery.min.js'></script>
+    <link rel="stylesheet" href="custom.css" />
+    <!-- <script src="assets/js/prefixfree.min.js"></script> -->
+</head>
+
+<body>
+    <div id="wrapper">
+        <div id="content">
+
+            <div id="card">
+                <div id="front">
+                    <div class="arrow">
+                        <a href="javascript:void(0);" class="more-info">
+                         <img src="arrow1.png">
+                        </a>
+                    </div>
+                    <div id="info-box">
+                        <div class="info">
+
+                        </div>
+                       <p class="top_san_name">
+                            <a href="javascript:void(0);">
+                              Pratik Boghani
+                            </a>
+                        </p>
+                        <p class="top_san_mob">
+                            <a href="tel:+918155858204" target="_blank">
+                              +91 81558 58204
+                            </a>
+                        </p>
+
+                    </div>
+
+                    <div id="social-bar">
+
+                      <a href="https://www.facebook.com/pratik.boghani.1?ref=bookmarks" target="_blank" class="facebook">
+                      <img src="facebook.png">
+                        </a>
+                        <a href="mailto:pratikboghani1@gmail.com" target="_blank" class="facebook">
+                          <img src="gmail.png">
+
+
+                        </a>
+                        <a href="https://www.instagram.com/mr.pratikboghani/" target="_blank"class="facebook">
+                          <img src="instagram.png">
+
+
+                        </a>
+                        <a href="https://wa.me/+918155858204" target="_blank" class="facebook">
+                            <img src="Whatsapp.png">                        </a>
+                        <a href="https://www.snapchat.com/add/pratikboghani" target="_blank" class="facebook">
+                            <img src="snapchat.png">
+                        </a>
+
+                       <!--  <a href="javascript:void(0);" class="more-info">
+                            <i class="fa fa-undo ffa"></i>
+                        </a> -->
+
+                      <div class="share_this_whats">
+                        <a id="sharethis" href="javascript:void(0);" style="font-size:10px;color:#ff6100;">
+
+                          <i style="font-size:15px;"></i>
+                          &nbsp;Share this on Whatsapp
+                        </a>
+                      </div>
+                      <div class="add_info">
+                          <div class="back-info">
+                            <p>
+                                <img src="call.png"></i>&nbsp;
+                                <a href="tel:+918155858204" target="_blank">
+                                  +91 81558 58204
+                                </a>
+                            </p>
+                            <p>
+                                <img src="call.png"></i>&nbsp;
+                                <a href="tel:+918160288326" target="_blank">
+                                  +91 81602 88326
+                                </a>
+                            </p>
+                            <p>
+
+                              <img src="mail.png">  &nbsp;
+                                <a href="mailto:pratikboghani1@gmail.com " target="_blank">
+                                  pratikboghani1@gmail.com
+                                </a>
+                            </p>
+                            <p>
+                              <img src="telegram.png">  &nbsp;
+                                <a href="http://t.me/ptatikboghani" target="_blank">
+                                  @pratikboghani
+                                </a>
+                            </p>
+                            <p>
+                                <img src="placeholder.png"></i>&nbsp;
+                                <a href="https://goo.gl/maps/GxMp2VD3jZf3RQyr6" target="_blank" class="front_add">
+
+                                  10,Prabhunagar-2, Katargam Darwaja, Surat, Gujarat 395004
+                                </a>
+                            </p>
+                            <p>
+                          </div>
+                      </div>
+                    <div class="save_contact">
+                          <a href="Pratik_Boghani.vcf" download title="Save Contact">
+                              <i style="font-size:15px;"></i> <img src="add-contact.png"> &nbsp; Save Contact
+                          </a>
+                          <script>
+                              $("#sharethis").on("click", function() {
+                                  var txt;
+                                  var person = prompt("Please Enter number to share:", "");
+                                  if (person == null || person == "") {
+                                      alert("Please Enter Mobile Number.");
+                                  } else if (isNaN(person) || person.length != 10) {
+                                      alert("Please Enter Only 10 digit Mobile Number.");
+                                  } else {
+                                      window.open("https://wa.me/91" + person + "?text=👉🏻Pratik Boghani Contact Card : Pratik_Boghani.vcf", "_BLANK");
+                                  }
+                              });
+                          </script>
+                      </div>
+                     <!--  <div class="click_flip">
+                           <i class="fa fa-save ffa"></i>
+                      </div> -->
+                    </div>
+                </div>
+                <div id="back">
+                  <div class="arrow1">
+                      <a href="javascript:void(0);" class="more-info">
+                        <img src="arrow2.png"></i>
+                      </a>
+                    </div>
+                    <div id="info-box">
+                        <div class="info">
+
+                        </div>
+                       <p class="top_san_name">
+                          <a href="javascript:void(0);">
+                            Jaydip Boghani
+                          </a>
+                      </p>
+                       <p class="top_ravi_mob">
+                            <a href="tel:+919737004971" target="_blank">
+                              +91 97370 04971
+                            </a>
+                        </p>
+                    </div>
+
+                    <div class="back-info-back" >
+                        <p>   <div class="tra" >
+                            <i class="fa fa-arrow-circle-right"></i>&nbsp;
+                            <a href="login.php" target="_blank" class="facebook">
+                              Birth Certificate
+                            </a>
+                          </div>
+                        </p>
+                        <p>
+                            <i class="fa fa-arrow-circle-right"></i>&nbsp;
+                            <a href="https://drive.google.com/file/d/1-9R9FlRYbTiiX7Do7Np4hukR9Umduk9I/view?usp=drivesdk" target="_blank">
+                              S.S.C & H.S.C
+                            </a>
+                        </p>
+                        <p>
+                            <i class="fa fa-arrow-circle-right"></i>&nbsp;
+                            <a href="https://drive.google.com/file/d/1oFF4t0sPLatu_cMbkq0eraQDwRMVNIxv/view?usp=drivesdk" target="_blank">
+                              PAN Card
+                            </a>
+                        </p>
+                        <p>
+                            <i class="fa fa-arrow-circle-right"></i>&nbsp;
+                            <a href="http://techbitinfo.com/software-development-company-in-surat" target="_blank">
+                              Voter Id
+                            </a>
+                        </p>
+                        <p>
+                            <i class="fa fa-arrow-circle-right"></i>&nbsp;
+                            <a href="https://drive.google.com/file/d/1-HPOjexKA7D1CqHNYhZRdl2W5rmtNoay/view" target="_blank">
+                              Aadhar Card
+                            </a>
+                        </p>
+                        <p>
+                            <i class="fa fa-arrow-circle-right"></i>&nbsp;
+                            <a href="https://drive.google.com/file/d/1-87qxcQZppEKHP8AmkO71ExyYw5zStIu/view?usp=drivesdk" target="_blank">
+                              School leaving Certificate
+                            </a>
+                        </p>
+                        <p>
+                            <i class="fa fa-arrow-circle-right"></i>&nbsp;
+                            <a href="https://drive.google.com/file/d/1-CY5Lu9NYnEWjdEnF1nNPzkvjQRv0zYf/view?usp=drivesdk" target="_blank">
+                             CCC certificate
+                            </a>
+                        </p>
+                        <p>
+                            <i class="fa fa-arrow-circle-right"></i>&nbsp;
+                            <a href="https://twitter.com/PratikBoghani" target="_blank">
+                             Twitter
+                            </a>
+                        </p>
+                        <p>
+                            <i class="fa fa-arrow-circle-right"></i>&nbsp;
+                            <a href="https://drive.google.com/file/d/1-QXrvW5Ez6LePV-1t2jGIqESXCvBX_cz/view?usp=drivesdk" target="_blank">
+                            Paytm
+                            </a>
+                        </p>
+
+                    </div>
+                  <!--  <div class="social-bar-back">
+                        <a href="https://twitter.com/PratikBoghani" target="_blank">
+                            <i class="fa fa-globe "></i>   twitter
+                        </a>
+
+                    </div>  -->
+                </div>
+            </div>
+            <div id="background">
+                <div id="background-image"></div>
+            </div>
+        </div>
+    </div>
+    <script src="index.js"></script>
+<script>
+/*document.onkeydown = function(e) {
+        if (e.ctrlKey &&
+            (e.keyCode === 67 ||
+             e.keyCode === 86 ||
+             e.keyCode === 85 ||
+             e.keyCode === 117)) {
+            return false;
+        } else {
+            return true;
+        }
+};
+
+$(document).keydown(function(event){
+    if(event.keyCode==123){
+    return false;
+   }
+else if(event.ctrlKey && event.shiftKey && event.keyCode==73){
+      return false;  //Prevent from ctrl+shift+i
+   }
+});
+
+$(document).on("contextmenu",function(e){
+   e.preventDefault();
+});
+   var checkStatus;
+*/
+</script>
+</body>
+</html>
